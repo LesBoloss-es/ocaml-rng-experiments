020f0e7483f7386411c19d0630e8087775c9bb94
Niols <niols@niols.fr>
2020-04-21 01:38:08 +0200

    benchmark bits from functorized versions

--- ocaml/bench.ml
+++ ocaml/bench.ml
@@ -2,29 +2,34 @@ open Xoshiro
 module B = Core_bench.Bench
 module Command = Core.Command
 
-let bits () =
-  Command.run (B.make_command [
-    B.Test.create ~name:"Random.bits" (fun () ->
-      ignore (Random.bits ()));
-
-    B.Test.create ~name:"X256pp.Int.bits" (fun () ->
-      ignore (X256pp.Int.bits ()));
-
-    B.Test.create ~name:"X256pp.Int64.bits" (fun () ->
-      ignore (X256pp.Int64.bits ()));
-    B.Test.create ~name:"X256pp.Int64.bits62" (fun () ->
-      ignore (X256pp.Int64.bits62 ()));
-
-    B.Test.create ~name:"X256pp.C.bits" (fun () ->
-      ignore (X256pp.C.bits ()));
-    B.Test.create ~name:"X256pp.C.bits62" (fun () ->
-      ignore (X256pp.C.bits62 ()));
-  ])
-
 module Random_Stdlib       = RandomFunctor.Basic(Random)
 module Random_X256pp_Int   = RandomFunctor.Basic(X256pp.Int)
 module Random_X256pp_Int64 = RandomFunctor.Basic(X256pp.Int64)
 module Random_X256pp_C     = RandomFunctor.Basic(X256pp.C)
+
+let bits () =
+  Command.run @@ B.make_command [
+    B.Test.create ~name:"Random.bits" (fun () ->
+        ignore (Random.bits ()));
+    B.Test.create ~name:"X256pp.Int.bits" (fun () ->
+        ignore (X256pp.Int.bits ()));
+    B.Test.create ~name:"X256pp.Int64.bits" (fun () ->
+        ignore (X256pp.Int64.bits ()));
+    B.Test.create ~name:"X256pp.Int64.bits62" (fun () ->
+        ignore (X256pp.Int64.bits62 ()));
+    B.Test.create ~name:"X256pp.C.bits" (fun () ->
+        ignore (X256pp.C.bits ()));
+    B.Test.create ~name:"X256pp.C.bits62" (fun () ->
+        ignore (X256pp.C.bits62 ()));
+    B.Test.create ~name:"RandomFunctor(Random).bits" (fun () ->
+        ignore (Random_Stdlib.bits ()));
+    B.Test.create ~name:"RandomFunctor(X256pp.Int).bits" (fun () ->
+        ignore (Random_X256pp_Int.bits ()));
+    B.Test.create ~name:"RandomFunctor(X256pp.Int64).bits" (fun () ->
+        ignore (Random_X256pp_Int64.bits ()));
+    B.Test.create ~name:"RandomFunctor(X256pp.C).bits" (fun () ->
+        ignore (Random_X256pp_C.bits ()));
+  ]
 
 let float () =
   Command.run @@ B.make_command [
