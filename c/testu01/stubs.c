85a0ca1e2c12e71ada50b71d9d8bc5c6b69acada
Niols <niols@niols.fr>
2020-04-26 16:34:45 +0200

    *crush & diehard

--- c/testu01/stubs.c
+++ c/testu01/stubs.c
@@ -14,10 +14,48 @@ unif01_Gen unif01_CreateExternGenFromCamlBits(value name, value bits) {
   return unif01_CreateExternGenBits(String_val(name), camlBits);
 }
 
+/* ***************************** [ SmallCrush ] ***************************** */
+
 value caml_bbattery_SmallCrush(value name, value bits) {
   unif01_Gen *gen;
   gen = unif01_CreateExternGenFromCamlBits(name, bits);
   bbattery_SmallCrush(gen);
+  unif01_DeleteExternGenBits(gen);
+  return Val_unit;
+}
+
+value caml_bbattery_SmallCrushFile(value filename) {
+  bbattery_SmallCrushFile(String_val(filename));
+  return Val_unit;
+}
+
+/* ******************************* [ Crush ] ******************************** */
+
+
+value caml_bbattery_Crush(value name, value bits) {
+  unif01_Gen *gen;
+  gen = unif01_CreateExternGenFromCamlBits(name, bits);
+  bbattery_Crush(gen);
+  unif01_DeleteExternGenBits(gen);
+  return Val_unit;
+}
+
+/* ****************************** [ BigCrush ] ****************************** */
+
+value caml_bbattery_BigCrush(value name, value bits) {
+  unif01_Gen *gen;
+  gen = unif01_CreateExternGenFromCamlBits(name, bits);
+  bbattery_BigCrush(gen);
+  unif01_DeleteExternGenBits(gen);
+  return Val_unit;
+}
+
+/* ****************************** [ DIEHARD ] ******************************* */
+
+value caml_bbattery_pseudoDIEHARD(value name, value bits) {
+  unif01_Gen *gen;
+  gen = unif01_CreateExternGenFromCamlBits(name, bits);
+  bbattery_pseudoDIEHARD(gen);
   unif01_DeleteExternGenBits(gen);
   return Val_unit;
 }
