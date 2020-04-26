c8e5a1be88c8d0223e509dbd8fa304db0865a341
Niols <niols@niols.fr>
2020-04-26 16:34:25 +0200

    cleanup

--- c/testu01/stubs.c
+++ c/testu01/stubs.c
@@ -5,27 +5,19 @@
 #include <caml/memory.h>
 #include <caml/callback.h>
 
-value * caml_bits_closure = NULL;
-
-unsigned int bits() {
-  return Int_val(caml_callback(*caml_bits_closure, Val_unit));
+value * camlBits_closure = NULL;
+unsigned int camlBits() {
+  return Int_val(caml_callback(*camlBits_closure, Val_unit));
+}
+unif01_Gen unif01_CreateExternGenFromCamlBits(value name, value bits) {
+  camlBits_closure = &bits;
+  return unif01_CreateExternGenBits(String_val(name), camlBits);
 }
 
-value caml_bbattery_SmallCrush(value name, value bits_closure) {
-  caml_bits_closure = &bits_closure;
-
+value caml_bbattery_SmallCrush(value name, value bits) {
   unif01_Gen *gen;
-  gen = unif01_CreateExternGenBits(String_val(name), bits);
+  gen = unif01_CreateExternGenFromCamlBits(name, bits);
   bbattery_SmallCrush(gen);
   unif01_DeleteExternGenBits(gen);
-
   return Val_unit;
-}
-
-value call_caml_f_ex(value closure, value arg)
-{
-  CAMLparam2(closure, arg);
-  CAMLlocal2(res, tmp);
-  res = caml_callback(closure, arg);
-  CAMLreturn (res);
 }
