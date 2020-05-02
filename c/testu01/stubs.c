59643000b344a7e9909ed21b15868e36602dcd37
Niols <niols@niols.fr>
2020-05-03 00:56:43 +0200

    custom_gen -> bgen

--- c/testu01/stubs.c
+++ c/testu01/stubs.c
@@ -10,8 +10,8 @@
 #include <caml/bigarray.h>
 #include <caml/custom.h>
 
-static struct custom_operations unif01_Gen_custom_ops = {
- identifier  : "unif01_Gen handling",
+static struct custom_operations unif01_Gen_boxed = {
+ identifier  : "unif01_Gen_boxed",
  finalize    : custom_finalize_default,
  compare     : custom_compare_default,
  hash        : custom_hash_default,
@@ -19,28 +19,30 @@ static struct custom_operations unif01_Gen_custom_ops = {
  deserialize : custom_deserialize_default
 };
 
-#define unif01_Gen_unbox(custom_gen) (* (unif01_Gen**) Data_custom_val(custom_gen))
+#define unif01_Gen_unbox(bgen) (* (unif01_Gen**) Data_custom_val(bgen))
 
-value camlBits_closure = 0;
+value camlBits_closure;
 unsigned int camlBits() {
   return Int_val(caml_callback(camlBits_closure, Val_unit));
 }
 value caml_unif01_CreateExternGenBits(value name, value bits) {
   CAMLparam2(name, bits);
-  CAMLlocal1(custom_gen);
+  CAMLlocal1(bgen);
 
   camlBits_closure = bits;
 
   unif01_Gen * gen;
   gen = unif01_CreateExternGenBits(Bytes_val(name), camlBits);
-  custom_gen = caml_alloc_custom(&unif01_Gen_custom_ops, sizeof(unif01_Gen*), 0, 1);
-  memcpy(Data_custom_val(custom_gen), &gen, sizeof(unif01_Gen*));
-  CAMLreturn(custom_gen);
+
+  bgen = caml_alloc_custom(&unif01_Gen_boxed, sizeof(unif01_Gen*), 0, 1);
+  memcpy(Data_custom_val(bgen), &gen, sizeof(unif01_Gen*));
+
+  CAMLreturn(bgen);
 }
 
-value caml_unif01_DeleteExternGenBits(value custom_gen) {
-  CAMLparam1(custom_gen);
-  unif01_Gen * gen = unif01_Gen_unbox(custom_gen);
+value caml_unif01_DeleteExternGenBits(value bgen) {
+  CAMLparam1(bgen);
+  unif01_Gen * gen = unif01_Gen_unbox(bgen);
   unif01_DeleteExternGenBits(gen);
   CAMLreturn(Val_unit);
 }
@@ -58,9 +60,9 @@ int * int_array_from_int32_bigarray(value ba) {
 
 /* ***************************** [ SmallCrush ] ***************************** */
 
-CAMLprim value caml_bbattery_SmallCrush(value custom_gen) {
-  CAMLparam1(custom_gen);
-  unif01_Gen * gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_SmallCrush(value bgen) {
+  CAMLparam1(bgen);
+  unif01_Gen * gen = unif01_Gen_unbox(bgen);
   bbattery_SmallCrush(gen);
   CAMLreturn(Val_unit);
 }
@@ -71,50 +73,50 @@ CAMLprim value caml_bbattery_SmallCrushFile(value filename) {
   CAMLreturn(Val_unit);
 }
 
-CAMLprim value caml_bbattery_RepeatSmallCrush(value custom_gen, value rep) {
-  CAMLparam2(custom_gen, rep);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_RepeatSmallCrush(value bgen, value rep) {
+  CAMLparam2(bgen, rep);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_RepeatSmallCrush(gen, int_array_from_int32_bigarray(rep));
   CAMLreturn(Val_unit);
 }
 
 /* ******************************* [ Crush ] ******************************** */
 
-CAMLprim value caml_bbattery_Crush(value custom_gen) {
-  CAMLparam1(custom_gen);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_Crush(value bgen) {
+  CAMLparam1(bgen);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_Crush(gen);
   CAMLreturn(Val_unit);
 }
 
-CAMLprim value caml_bbattery_RepeatCrush(value custom_gen, value rep) {
-  CAMLparam2(custom_gen, rep);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_RepeatCrush(value bgen, value rep) {
+  CAMLparam2(bgen, rep);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_RepeatCrush(gen, int_array_from_int32_bigarray(rep));
   CAMLreturn(Val_unit);
 }
 
 /* ****************************** [ BigCrush ] ****************************** */
 
-CAMLprim value caml_bbattery_BigCrush(value custom_gen) {
-  CAMLparam1(custom_gen);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_BigCrush(value bgen) {
+  CAMLparam1(bgen);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_BigCrush(gen);
   CAMLreturn(Val_unit);
 }
 
-CAMLprim value caml_bbattery_RepeatBigCrush(value custom_gen, value rep) {
-  CAMLparam2(custom_gen, rep);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_RepeatBigCrush(value bgen, value rep) {
+  CAMLparam2(bgen, rep);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_RepeatBigCrush(gen, int_array_from_int32_bigarray(rep));
   CAMLreturn(Val_unit);
 }
 
 /* ******************************* [ Rabbit ] ******************************* */
 
-CAMLprim value caml_bbattery_Rabbit(value custom_gen, value nb) {
-  CAMLparam2(custom_gen, nb);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_Rabbit(value bgen, value nb) {
+  CAMLparam2(bgen, nb);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_Rabbit (gen, Double_val(nb));
   CAMLreturn(Val_unit);
 }
@@ -125,18 +127,18 @@ CAMLprim value caml_bbattery_RabbitFile(value filename, value nb) {
   CAMLreturn(Val_unit);
 }
 
-CAMLprim value caml_bbattery_RepeatRabbit(value custom_gen, value nb, value rep) {
-  CAMLparam3(custom_gen, nb, rep);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_RepeatRabbit(value bgen, value nb, value rep) {
+  CAMLparam3(bgen, nb, rep);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_RepeatRabbit(gen, Double_val(nb), int_array_from_int32_bigarray(rep));
   CAMLreturn(Val_unit);
 }
 
 /* ****************************** [ Alphabit ] ****************************** */
 
-CAMLprim value caml_bbattery_Alphabit(value custom_gen, value nb, value r, value s) {
-  CAMLparam4(custom_gen, nb, r, s);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_Alphabit(value bgen, value nb, value r, value s) {
+  CAMLparam4(bgen, nb, r, s);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_Alphabit (gen, Double_val(nb), Int_val(r), Int_val(s));
   CAMLreturn(Val_unit);
 }
@@ -147,16 +149,16 @@ CAMLprim value caml_bbattery_AlphabitFile(value filename, value nb) {
   CAMLreturn(Val_unit);
 }
 
-CAMLprim value caml_bbattery_RepeatAlphabit(value custom_gen, value nb, value r, value s, value rep) {
-  CAMLparam5(custom_gen, nb, r, s, rep);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_RepeatAlphabit(value bgen, value nb, value r, value s, value rep) {
+  CAMLparam5(bgen, nb, r, s, rep);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_RepeatAlphabit(gen, Double_val(nb), Int_val(r), Int_val(s), int_array_from_int32_bigarray(rep));
   CAMLreturn(Val_unit);
 }
 
-CAMLprim value caml_bbattery_BlockAlphabit(value custom_gen, value nb, value r, value s) {
-  CAMLparam4(custom_gen, nb, r, s);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_BlockAlphabit(value bgen, value nb, value r, value s) {
+  CAMLparam4(bgen, nb, r, s);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_BlockAlphabit (gen, Double_val(nb), Int_val(r), Int_val(s));
   CAMLreturn(Val_unit);
 }
@@ -167,9 +169,9 @@ CAMLprim value caml_bbattery_BlockAlphabitFile(value filename, value nb) {
   CAMLreturn(Val_unit);
 }
 
-CAMLprim value caml_bbattery_RepeatBlockAlphabit(value custom_gen, value nb, value r, value s, value rep, value w) {
-  CAMLparam5(custom_gen, nb, r, s, rep); CAMLxparam1(w);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_RepeatBlockAlphabit(value bgen, value nb, value r, value s, value rep, value w) {
+  CAMLparam5(bgen, nb, r, s, rep); CAMLxparam1(w);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_RepeatBlockAlphabit(gen, Double_val(nb), Int_val(r), Int_val(s), int_array_from_int32_bigarray(rep), Int_val(w));
   CAMLreturn(Val_unit);
 }
@@ -179,18 +181,18 @@ CAMLprim value camlbytecode_bbattery_RepeatBlockAlphabit(value * argv, int argn)
 
 /* ****************************** [ DIEHARD ] ******************************* */
 
-CAMLprim value caml_bbattery_pseudoDIEHARD(value custom_gen) {
-  CAMLparam1(custom_gen);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_pseudoDIEHARD(value bgen) {
+  CAMLparam1(bgen);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_pseudoDIEHARD(gen);
   CAMLreturn(Val_unit);
 }
 
 /* **************************** [ FISP 140 2 ] ****************************** */
 
-CAMLprim value caml_bbattery_FIPS_140_2(value custom_gen) {
-  CAMLparam1(custom_gen);
-  unif01_Gen *gen = unif01_Gen_unbox(custom_gen);
+CAMLprim value caml_bbattery_FIPS_140_2(value bgen) {
+  CAMLparam1(bgen);
+  unif01_Gen *gen = unif01_Gen_unbox(bgen);
   bbattery_FIPS_140_2(gen);
   CAMLreturn(Val_unit);
 }
